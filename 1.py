bc5e87a00f762c5f6c1657cb9f0b8ceef16e40c2
rival syahmidan <65500976+flamousz@users.noreply.github.com>
2020-05-17 23:26:53 +0700

    boolean op procedures

---
+++ 1.py
@@ -0,0 +1,23 @@
+num = 4
+if num==4:
+    print('number is 4')
+else:
+    if num==5:
+        print('number is 5')
+    else:
+        if num==3:
+            print('number is 3')
+        else:
+            print('number is not 3, 4 or 5')
+
+
+num = 5
+if num==4:
+    print('number is 4')
+elif num==6:
+    print('number is 6')
+elif num==7:
+    print('number is 7')
+elif num==5:
+    print('number is 5')
+else:
