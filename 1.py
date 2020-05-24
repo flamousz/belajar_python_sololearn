0e861e4c0248c7368dd449db56140b23d729bd45
rival syahmidan <65500976+flamousz@users.noreply.github.com>
2020-05-25 01:20:31 +0700

    Modules
    Modules are pieces of code that other people have written to fulfill common tasks, 
    such as generating random numbers, performing mathematical operations, etc

--- 1.py
+++ 1.py
@@ -761,6 +761,47 @@ tes(square,5)
 # 24 5 20 02.36 istirahat
 
 
+# 24 5 20 01.16 start
+# Function as objects
+# 24 5 20 02.36 istirahat
+# 25 5 20 00.30 start
+
+# Modules
+'''
+Modules are pieces of code that other people have written to fulfill common tasks, 
+such as generating random numbers, performing mathematical operations, etc.
+The basic way to use a module is to add import module_name at the top of your code,  
+and then using module_name.var to access functions and values with the name var in the module.
+For example, the following example uses the random module to generate random numbers. '''
+
+# ex 1
+import random
+
+for i in range(4):
+    value = random.randint(3, 9)
+    print(value)
+
+'''
+The code uses the randint function defined in the random module to print 5 random numbers in the range 1 to 6.
+
+There is another kind of import that can be used if you only need certain functions from a module.
+These take the form from module_name import var, and then var can be used as if it were defined normally in your code. 
+For example, to import only the pi constant from the math module. '''
+# ex 2
+from math import pi, sqrt
+print(pi)
+print(sqrt(3))
+
+'''
+You can import a module or object under a different name using the as keyword. 
+This is mainly used when a module or object has a long or confusing name. '''
+# ex 3
+from math import pi as a
+print(a)
+# 25 5 20 00.57 istirahat
+
+
+
 
 
 
