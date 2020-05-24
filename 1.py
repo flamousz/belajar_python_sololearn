32118d596850274122d120bb37a52c35b6ba4aff
rival syahmidan <65500976+flamousz@users.noreply.github.com>
2020-05-25 01:22:41 +0700

    The standard library and pip
    Tasks that can be done by the standard library include string parsing, data serialization, 
    testing, debugging and manipulating dates, emails, command line arguments, and much more!

--- 1.py
+++ 1.py
@@ -800,6 +800,28 @@ from math import pi as a
 print(a)
 # 25 5 20 00.57 istirahat
 
+# 25 5 20 00.58 start
+
+# The standard library and pip
+'''
+There are three main types of modules in Python, those you write yourself, 
+those you install from external sources, and those that are preinstalled with Python. 
+The last type is called the standard library, and contains many useful modules. 
+Some of the standard library's useful modules include string, re, datetime, math, 
+random, os, multiprocessing, subprocess, socket, email, json, doctest, unittest, 
+pdb, argparse and sys.
+
+Tasks that can be done by the standard library include string parsing, data serialization, 
+testing, debugging and manipulating dates, emails, command line arguments, and much more! 
+
+some of the modules in the standard library are written in python, and some are written in C'''
+
+# 25 5 20 01.16 istirahat
+
+~                                                                                                                                                     
+~           
+
+
 
 
 
